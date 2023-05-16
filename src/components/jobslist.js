594e22212fe343c7aefa6ff9b11d13b19bcc30e1
luccasrojas <l.rojasb@uniandes.edu.co>
2023-05-16 09:19:36 -0500

    actividad completada

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import Job from "./job";
+import { FormattedDate } from "react-intl";
+import { FormattedMessage } from "react-intl";
 
-const JobsList = () => {
+const JobsList = (props) => {
+  console.log(props)
   const [offers] = useState([
     {
       id: "0001",
@@ -10,6 +13,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: "123428"
     },
     {
       id: "0002",
@@ -18,6 +22,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: "2423012"
     },
     {
       id: "0003",
@@ -26,20 +31,41 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: "9234028"
     },
   ]);
+  //Si props esta en español poner el fondo blanco, si esta en ingles en negro
+  const renderLang = () => {
+    if (props.lang=="es" ||props.lang=="es-ES" )
+    {return "table-light"}
+    else if (props.lang=="en"||props.lang=="en-EN")
+    {return "table-dark"}
+  };
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+        <thead className={renderLang()}>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="PublicationDate" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Views" />
+            </th>
           </tr>
         </thead>
         <tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+// Determinar el idioma del navegador
+const userLanguage = navigator.language || navigator.userLanguage;
+console.log(userLanguage)
+// Configurar los mensajes en base al idioma
+let messages;
+switch (userLanguage) {
+    case 'en-EN' || 'en':
+        messages = require('./locales/en.json');
+        break;
+    case 'es-ES' || 'es':
+        messages = require('./locales/es.json');
+        break;
+    // Agrega más casos según los idiomas que desees soportar
+    default:
+        messages = require('./locales/en.json');
+        break;
+    }
+
+ReactDOM.render(
+    <IntlProvider locale={userLanguage} messages={messages}>
+            <JobsList lang={userLanguage} />
+    </IntlProvider>, document.getElementById("root")
+);
